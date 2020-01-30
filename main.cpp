2c3ffbf54b04133e2a2a9fde146f8cdb312703ca
raviverma2791747 <55842522+raviverma2791747@users.noreply.github.com>
2020-01-30 16:21:50 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,27 +7,55 @@ class ic
     int x;
     int y;
     int z;
+    int w;
+    int i;
     public:
 
     void input(int a,int b,int c)
     {
         x=a;
         y=b;
-        if(c == 0)
+        i=c;
+        if(i == 0)
         {
-            dot();
+            _and();
         }
-        else if(c == 1)
+        else if(i == 1)
         {
             _or();
         }
-        else
+        else if(i == 2)
         {
             _xor();
         }
+        else if(i == 3)
+        {
+            _nand();
+        }
+        else
+        {
+            _nor();
+        }
+    }
+    void _not()
+    {
+        if(w==0)
+        {
+            z=1;
+        }
+        else
+        {
+            z=0;
+        }
     }
 
-    void dot()
+    void input(int n)
+    {
+        w = n;
+        _not();
+    }
+
+    void _and()
     {
         z =  x*y;
     }
@@ -56,6 +84,20 @@ class ic
         }
     }
 
+    void _nor()
+    {
+        _or();
+        w = z;
+        _not();
+    }
+
+    void _nand()
+    {
+        _and();
+        w = z;
+        _not();
+    }
+
     int result()
     {
         return z;
@@ -63,9 +105,9 @@ class ic
 
 };
 
-int main()
+void full_adder()
 {
-    ic ic_and0,ic_and1,ic_xor0,ic_xor1,ic_or0;
+     ic ic_and0,ic_and1,ic_xor0,ic_xor1,ic_or0;
     int a=0,b=0,c=0;
     cout<<"Enter the two inputs\n";
     cin>>a>>b>>c;
@@ -75,5 +117,45 @@ int main()
     ic_and1.input(a,b,0);
     ic_or0.input(ic_and0.result(),ic_and1.result(),1);
     cout<<ic_or0.result()<<ic_xor1.result();
+}
+
+void multiplexer()
+{
+    ic ic_nand0,ic_nand1,ic_nand2,ic_nand3;
+    int a=0,i=0,_i=0;
+    cin>>_i>>a>>i;
+    ic_nand0.input(a,_i,3);
+    ic_nand1.input(a,a,3);
+    ic_nand2.input(i,ic_nand1.result(),3);
+    ic_nand3.input(ic_nand0.result(),ic_nand2.result(),3);
+    cout<<ic_nand3.result();
+}
+
+void comparator()
+{
+    ic ic_not0,ic_not1,ic_and0,ic_and1,ic_or0;
+    int a =1, b = 0;
+    ic_not0.input(a);
+    ic_not1.input(b);
+    ic_and0.input(ic_not0.result(),b,0);
+    ic_and1.input(ic_not1.result(),a,0);
+    ic_or0.input(ic_and0.result(),ic_and1.result(),1);
+    if(ic_and0.result() ==  1)
+    {
+        cout<<"A<B";
+    }
+    else if(ic_and1.result() == 1)
+    {
+        cout<<"A>B";
+    }
+    else
+    {
+        cout<<"A=B";
+    }
+
+}
+int main()
+{
+   comparator();
     return 0;
 }
